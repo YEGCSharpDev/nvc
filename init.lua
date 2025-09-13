00058571510d9b5ac88a3a1507da39960379caf8
Shankara Subramanian <shankara.subramanian@wcb.ab.ca>
2025-09-13 11:57:32 -0600

    working config

--- init.lua
+++ init.lua
@@ -35,3 +35,21 @@ require "autocmds"
 vim.schedule(function()
   require "mappings"
 end)
+
+vim.opt.clipboard = "unnamedplus"
+
+if vim.fn.has('wsl') == 1 then
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+
+group = vim.api.nvim_create_augroup('Yank', { clear = true }),
+
+callback = function()
+
+vim.fn.system('clip.exe', vim.fn.getreg('"'))
+
+end,
+
+})
+
+end
