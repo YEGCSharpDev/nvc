c05144e7854739815f5c722b1c5e44edb4b47ada
Shankara Subramanian <shankara.subramanian@wcb.ab.ca>
2025-10-07 15:40:27 -0600

    orgmode working journal

--- lua/plugins/orgmode.lua
+++ lua/plugins/orgmode.lua
@@ -7,12 +7,12 @@ return {
       org_default_notes_file = "~/work/wiki/refile.org",
       org_todo_keywords = { "TODO(t)", "WAITING(w)", "NEXT(n)", "INPROGRESS(p)", "BLOCKED(b)", "|", "DONE(d)" },
       org_todo_keyword_faces = {
-        TODO = ":background #000000 :foreground red", -- overrides builtin color for `TODO` keyword
-        WAITING = ":background #000000 :foreground #cc6216",
-        NEXT = ":background #000000 :foreground #a911cf :weight bold",
-        INPROGRESS = ":background #000000 :foreground #16ccc6 :weight bold",
-        DONE = ":background green :slant italic :underline on",
-        BLOCKED = ":background #000000 :foreground #b31414 :weight bold :slant italic :underline on",
+        TODO = ":foreground #000000 :background red", -- overrides builtin color for `TODO` keyword
+        WAITING = ":foreground #000000 :background #cc6216",
+        NEXT = ":foreground #000000 :background #a911cf :weight bold",
+        INPROGRESS = ":foreground #000000 :background #16ccc6 :weight bold",
+        DONE = ":foreground #000000 :background green :slant italic :underline on",
+        BLOCKED = ":foreground #000000 :background #b31414 :weight bold :slant italic :underline on",
       },
       org_log_done = "note",
       org_log_into_drawer = "LOGBOOK",
@@ -25,19 +25,10 @@ return {
           template = "* TODO %?\n %u",
           target = "~/work/todo.org",
         },
-        j = { --Would be nice to have a subtemplate. 1 to create today's note and 1 to add to it
-          description = "Journal",
-          reversed = true,
-          tree_type = "day",
-          template = "\n*** %<%Y-%m-%d> %<%A>\n**** %U\n\n%?",
-          target = "~/work/dailies/journal.org",
-        },
-        w = { --Would be nice to have a subtemplate. 1 to create today's note and 1 to add to it
-          description = "Day",
-          reversed = true,
-          datetree = { tree_type = "week" },
---          template = "\n*** %<%Y-%m-%d> %<%A>\n**** %U\n\n%?",
-          target = "~/work/dailies/test.org",
+      j = {
+          description = "Daily Journal",
+          template = "*%?",
+          target = "~/work/journal/%<%Y>/%<%V>/%<%Y-%m-%d>-%<%A>.org",
         },
       },
     }
