75de18057e3c7906b1df2ce79dd6fb6628e925a3
Shankara Subramanian <shankara.subramanian@wcb.ab.ca>
2025-10-07 13:27:20 -0600

    OrgMode working really well

--- lua/plugins/orgmode.lua
+++ lua/plugins/orgmode.lua
@@ -14,6 +14,32 @@ return {
         DONE = ":background green :slant italic :underline on",
         BLOCKED = ":background #000000 :foreground #b31414 :weight bold :slant italic :underline on",
       },
+      org_log_done = "note",
+      org_log_into_drawer = "LOGBOOK",
+      org_adapt_indentation = true,
+      win_split_mode = "float",
+      org_capture_templates = {
+        t = {
+          description = "Todo",
+          reversed = true,
+          template = "* TODO %?\n %u",
+          target = "~/work/todo.org",
+        },
+        j = { --Would be nice to have a subtemplate. 1 to create today's note and 1 to add to it
+          description = "Journal",
+          reversed = true,
+          tree_type = "day",
+          template = "\n*** %<%Y-%m-%d> %<%A>\n**** %U\n\n%?",
+          target = "~/work/dailies/journal.org",
+        },
+        w = { --Would be nice to have a subtemplate. 1 to create today's note and 1 to add to it
+          description = "Day",
+          reversed = true,
+          datetree = { tree_type = "week" },
+--          template = "\n*** %<%Y-%m-%d> %<%A>\n**** %U\n\n%?",
+          target = "~/work/dailies/test.org",
+        },
+      },
     }
   end,
 }
