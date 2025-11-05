188a22405040c5ab32fe9f0d93233f5eaff79b66
Shankara Subramanian <shankara.subramanian@wcb.ab.ca>
2025-11-04 20:57:25 -0700

    new mapping

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,4 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 map({ "n", "i", "v" }, "<C-S>", "<cmd> wa <cr>")
+map({ "n", "i", "v" }, "<leader>rr", "<cmd> %s/\\r//g<cr>",{desc = "replace artifacts left from pasting"})
